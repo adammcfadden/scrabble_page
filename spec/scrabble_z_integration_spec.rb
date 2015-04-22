877dae9583f948042c43c2037af913a2c35e38c7
Toby Alden & Adam McFadden <student@epicodus.com>
2015-04-22 11:36:59 -0700

    Clone scrabble-master. Add integration spec & empty .erb files.

---
+++ spec/scrabble_z_integration_spec.rb
@@ -0,0 +1,12 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app  = Sinatra::Application
+
+describe('the scrabble path', {:type => :feature}) do
+  it("takes a word as input and returns the sum of the letters' scrabble scores.") do
+    visit('/')
+    fill_in('word', :with => 'path')
+    click_button('submit')
+    expect(page).to(have_content('That word would score 9'))
+  end
+end
